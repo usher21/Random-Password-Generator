87ad651b568c4d0d6aa91f547984954ff8dced8f
Mar Ndiaye <mar.ndiaye221@gmail.com>
2023-03-12 18:27:50 +0000

    Modification des messages d'alert et changement de la génération du mot de passe

--- script.js
+++ script.js
@@ -6,61 +6,74 @@ const copyIcon = document.querySelector('.input-container i')
 const toolTip = document.querySelector('.input-container .tooltip')
 
 const alertContainer = document.querySelector('.alert-container')
+const alertDanger = alertContainer.querySelector('.danger')
+const alertText = alertContainer.querySelector('.icon-text .text')
+const alertCloseBtn = alertContainer.querySelector('.alert .close')
 
 passwdInput.value = ''
-passwdLength.value = '15'
+passwdLength.value = '1'
 
 copyIcon.addEventListener('click', () => {
     navigator.clipboard.writeText(passwdInput.value)
     toolTip.classList.add('active')
     setTimeout(() => toolTip.classList.remove('active'), 2000);
 })
-passwdLength.addEventListener('keyup', () => toggleStateButton())
+passwdLength.addEventListener('keyup', toggleStateButton)
 
 generatePasswdBtn.addEventListener('click', () => {
-    let checkedElements = Array.from(inputCheck).filter(element => element.checked)
-    let checkCount = checkedElements.length
+    let checkCount = Array
+                        .from(inputCheck)
+                        .filter(element => element.checked)
+                        .length
     
     let length = Number(passwdLength.value)
 
-    if (length < 15 || length > 20) {
-        const alertDanger = createAlert('alert danger', 'fa-solid fa-circle-exclamation', 'Erreur ! La longeur du mot doit être compris entre 15 et 20')
-        alertContainer.append(alertDanger)
-        setTime(alertDanger)
-        passwdInput.value = ''
-        return        
-    }
+    if (!checkPasswordLength(length))
+        return
 
     if (checkCount == 0) {
-        const alertDanger = createAlert('alert danger', 'fa-solid fa-circle-exclamation', 'Erreur ! Veuillez séléctionner un critère')
-        alertContainer.append(alertDanger)
-        setTime(alertDanger)
-        passwdInput.value = ''
+        showErrorAlert('Erreur ! Veuillez séléctionner un critère')
+    } else if(checkCount > length) {
+        showErrorAlert('La longueur du mot de passe ne doit pas être inférieure au nombre de case coché !!!')
     } else {
-        passwdInput.value = generatePassword(length, checkedElements, checkCount)
+        passwdInput.value = generatePassword(length)
     }
 })
 
-function generatePassword(length, checkedElements, checkCount) {
-    let passwordGenerated = ""
-    let index = 0
-    
-    for (let i = 0; i < length; i++) {
-        index = Math.floor(Math.random() * checkCount)
-        const element = checkedElements[index]
-        
-        if (element.getAttribute('name') === 'upper') {
+function generatePassword(length) {
+    let passwordGenerated = "", i = 0
+    const upperChecked = inputCheck[0].checked
+    const lowerChecked = inputCheck[1].checked
+    const numberChecked = inputCheck[2].checked
+    const specialChecked = inputCheck[3].checked
+
+    while (i < length) {
+        if (upperChecked && i < length) {
             passwordGenerated += generateUpper()
-        } else if (element.getAttribute('name') === 'lower') {
+            passwordGenerated = shufflePassword(passwordGenerated)
+            i++
+        }
+        if(lowerChecked && i < length) {
             passwordGenerated += generateLower()
-        } else if (element.getAttribute('name') === 'number') {
+            passwordGenerated = shufflePassword(passwordGenerated)
+            i++
+        }
+        if (numberChecked && i < length) {
             passwordGenerated += generateNumber()
-        } else if (element.getAttribute('name') === 'special') {
+            passwordGenerated = shufflePassword(passwordGenerated)
+            i++
+        }
+        if (specialChecked && i < length) {
             passwordGenerated += generateSpecial()
+            passwordGenerated = shufflePassword(passwordGenerated)
+            i++
         }
     }
-
     return passwordGenerated
+}
+
+function shufflePassword(passwd) {
+    return Array.from(passwd).sort(char => Math.random() - 0.5).join('')
 }
 
 function generateLower() {
@@ -80,44 +93,8 @@ function generateSpecial() {
     return special[Math.floor(Math.random() * special.length)]
 }
 
-function createElement(tagName, attributs = {}, content = '') {
-    const element = document.createElement(tagName)
-    for (const attribut in attributs) {
-        element.setAttribute(attribut, attributs[attribut])
-    }
-    element.textContent = content
-    return element
-}
-
-function createAlert(alertClassName, iconName, alertText) {
-    const alert = createElement('div', { class: alertClassName })
-
-    const textIcon = createElement('div', { class: 'icon-text' })
-    const icon = createElement('i', { class: iconName })
-    const text = createElement('span', { class: 'text' }, alertText)
-    textIcon.append(icon)
-    textIcon.append(text)
-    alert.append(textIcon)
-
-    return alert
-}
-
-function removeAlert(alert) {
-    alertContainer.removeChild(alert)
-}
-
-function toggleAlertState(alert) {
-    if (alert.classList.contains('active')) {
-        alert.classList.add('inactive')
-        alert.classList.remove('active')
-    } else {
-        alert.classList.add('active')
-        alert.classList.remove('inactive')
-    }
-}
-
 function toggleStateButton() {
-    if (Number(passwdLength.value) < 15 || Number(passwdLength.value) > 20) {
+    if (Number(passwdLength.value) < 1 || Number(passwdLength.value) > 20) {
         generatePasswdBtn.disabled = true
         generatePasswdBtn.classList.remove('active')
     } else {
@@ -126,12 +103,35 @@ function toggleStateButton() {
     }
 }
 
-function setTime(alertDanger) {
-    toggleAlertState(alertDanger)
-    setTimeout(() => {
-        toggleAlertState(alertDanger)
-        setTimeout(() => {
-            removeAlert(alertDanger)
-        }, 500);
-    } , 3000)
+function checkPasswordLength(length) {
+    if (isNaN(length)) {
+        showErrorAlert('Erreur ! veuillez saisir un nombre ')
+        return false
+    } else if (length < 1 || length > 20) {
+        showErrorAlert('Erreur ! La longeur du mot doit être compris entre 1 et 20')
+        return false
+    }
+    return true
+}
+
+function showErrorAlert(errorMessage) {
+    alertText.textContent = errorMessage
+    setTime()
+    passwdInput.value = ''
+}
+
+function removeAlert() {
+    alertDanger.classList.remove('active')
+    alertDanger.classList.add('inactive')
+}
+
+function activeAlert() {
+    alertDanger.classList.remove('inactive')
+    alertDanger.classList.add('active')
+}
+
+function setTime() {
+    activeAlert()
+    alertCloseBtn.addEventListener('click', removeAlert)
+    setTimeout(removeAlert, 3000)
 }
